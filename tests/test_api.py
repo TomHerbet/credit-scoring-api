766014522961cccfc7b6b43341bd88da0e9a6ead
TomHerbet <herbet.thomas@outlook.fr>
2026-01-06 19:04:08 +0100

    Fix: correction du path pour l'import de api_v4 depuis tests/

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,7 +7,9 @@ import sys
 import os
 
 # Ajout du chemin courant pour permettre l'import
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
 
 # Import de votre application
 import api_v4
